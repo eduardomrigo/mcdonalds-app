1bac7ab0761344de048ab525da113812e43b3be6
Eduardo Rigo <eduardorigom@gmail.com>
2025-02-20 12:16:08 -0300

    feat: add product details page

--- src/app/[slug]/menu/[productId]/page.tsx
+++ src/app/[slug]/menu/[productId]/page.tsx
@@ -3,7 +3,8 @@ import { db } from "@/lib/prisma";
 import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
 import Image from "next/image";
 import { notFound } from "next/navigation";
-import ProductHeader from "./components/productHeader";
+import ProductHeader from "./components/product-header";
+import ProductDetail from "./components/product-details";
 
 interface ProductPageProps {
     params: Promise<{ slug: string; productId: string }>
@@ -11,20 +12,34 @@ interface ProductPageProps {
 
 async function ProductPage({ params }: ProductPageProps) {
     const { slug, productId } = await params
+
     const product = await db.product.findUnique({
         where: {
             id: productId
+        },
+        include: {
+            restaurant: {
+                select:{
+                    name: true,
+                    avatarImageUrl: true,
+                    slug: true
+                }
+            }
         }
     })
 
     if (!product) {
         return notFound()
     }
+    if(product.restaurant.slug.toUpperCase() !== slug.toUpperCase()){
+        return notFound()
+    }
 
     return (
-        <>
+        <div className="flex h-full flex-col">
          <ProductHeader product={product} />
-        </>
+         <ProductDetail product={product} />
+        </div>
 
     )
 }
