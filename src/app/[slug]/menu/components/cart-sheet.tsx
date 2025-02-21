98329610e5f972daf6507a67854ac6f67b92eaff
Eduardo Rigo <eduardorigom@gmail.com>
2025-02-21 13:52:54 -0300

    feat: ensure cart total is shown

--- src/app/[slug]/menu/components/cart-sheet.tsx
+++ src/app/[slug]/menu/components/cart-sheet.tsx
@@ -2,20 +2,34 @@ import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle } from '
 import React, { useContext } from 'react'
 import { CartContext } from '../contexts/cart'
 import CartProductItem from './cart-product-item'
+import { Button } from '@/components/ui/button'
+import { Card, CardContent } from '@/components/ui/card'
+import { formatCurrency } from '@/helpers/fomat-currency'
 
 function CartSheet() {
 
-    const { isOpen, toggleCart, products } = useContext(CartContext)
+    const { isOpen, toggleCart, products, total } = useContext(CartContext)
     return (
         <Sheet open={isOpen} onOpenChange={toggleCart}>
             <SheetContent className='w-[80%]'>
                 <SheetHeader>
                     <SheetTitle className='text-left'>Sacola</SheetTitle>
                 </SheetHeader>
-                <div className='py-5 space-y-5'>
-                    {products.map((product) => (
-                        <CartProductItem key={product.id} product={product} />
-                    ))}
+                <div className='py-5 space-y-5 h-full flex flex-col'>
+                    <div className="flex-auto">
+                        {products.map((product) => (
+                            <CartProductItem key={product.id} product={product} />
+                        ))}
+                    </div>
+                    <Card className='mb-6'>
+                        <CardContent className='p-5'>
+                            <div className="flex justify-between">
+                                <p className='text-sm text-muted-foreground'>Total</p>
+                                <p className='font-semibold text-sm'>{formatCurrency(total)}</p>
+                            </div>
+                        </CardContent>
+                    </Card>
+                    <Button className='w-full rounded-full'>Finalizar pedido</Button>
                 </div>
             </SheetContent>
         </Sheet>
